cd2db219f3cec39c380ce4178e029a2492bb7073
daniel <duque1daniel@gmail.com>
2020-09-24 19:52:03 -0700

    fixed error

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 const display = document.querySelector('#display')
 const billInput = document.querySelector('#bill')
 const tipInput = document.querySelector('#tip')
+const peopleInput = document.querySelector('#people')
+const displayTotal = document.querySelector('#total')
 
 console.log(display.innerHTML) // prints the text inside the tags
 console.log(billInput.value) // prints the value entered into the input
 
-billInput.value = '' // Set the value to 999.88
+// billInput.value = '0'
+// tipInput.value = '0'
+// peopleInput.value = '0'
 
 function calculateTip() {
-    const billValue = billInput.value
-    const tipValue = tipInput.value
-
+    const billValue = parseFloat(billInput.value)
+    const tipValue = parseInt(tipInput.value)
+    const peopleValue = parseInt(peopleInput.value)
     const tipAmount = billValue * tipValue / 100
+    const totalValue = (billValue + parseInt(tipAmount)) / peopleValue
 
     display.innerHTML = tipAmount
+    displayTotal.innerHTML = totalValue
 
 
 }
 
 billInput.addEventListener('input', calculateTip)
 tipInput.addEventListener('input', calculateTip)
-
-calculateTip()
+peopleInput.addEventListener('input', calculateTip)
